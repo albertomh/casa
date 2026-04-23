ac8a3678272e8950599514ccb0b13f70b9686c5f
Alberto Morón Hernández <alberto.moronh@gmail.com>
2026-04-23 23:04:34 +0100

    refactor: encapsulate functionality in Freezer, FreezerRenderer classes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { DurableObject } from "cloudflare:workers";
+import FreezerFormHtml from "./templates/freezer/form.html";
+import HomepageHtml from "./templates/index.html";
 
 /**
  * Welcome to Cloudflare Workers! This is your first Durable Objects application.
@@ -15,7 +17,11 @@ import { DurableObject } from "cloudflare:workers";
 
 const DURABLE_OBJECT_NAME = "casa";
 
-import HomepageHtml from "./index.html";
+type FreezerItem = {
+    id: number;
+    name: string;
+    quantity: number;
+};
 
 function initDatabaseSchema(sql: SqlStorage): void {
     sql.exec(`CREATE TABLE IF NOT EXISTS freezer__items(
@@ -24,42 +30,73 @@ function initDatabaseSchema(sql: SqlStorage): void {
 		quantity INTEGER)`);
 }
 
-/** A Durable Object's behavior is defined in an exported Javascript class */
-export class CasaDurableObject extends DurableObject<Env> {
-    sql: SqlStorage;
-    /**
-     * The constructor is invoked once upon creation of the Durable Object, i.e. the first call to
-     * 	`DurableObjectStub::get` for a given identifier (no-op constructors can be omitted)
-     *
-     * @param ctx - The interface for interacting with Durable Object state
-     * @param env - The interface to reference bindings declared in wrangler.jsonc
-     */
-    constructor(ctx: DurableObjectState, env: Env) {
-        super(ctx, env);
-        this.sql = ctx.storage.sql;
+class Freezer {
+    constructor(private sql: SqlStorage) {}
 
-        initDatabaseSchema(this.sql);
+    listItems() {
+        return this.sql.exec("SELECT * FROM freezer__items").toArray();
     }
 
-    async getItems(): Promise<Response> {
-        const rows = this.sql.exec("SELECT * FROM freezer__items").toArray();
-
-        const html = rows
-            .map((r) => `<li>${r.name} (${r.quantity ?? 1})</li>`)
-            .join("");
-
-        return new Response(html, {
-            headers: { "Content-Type": "text/html" },
-        });
-    }
-
-    async addItem(name: string): Promise<Response> {
+    addItem(name: string) {
         this.sql.exec(
             "INSERT INTO freezer__items (name, quantity) VALUES (?, 1)",
             [name],
         );
+    }
+}
 
-        return this.getItems();
+class FreezerRenderer {
+    private escape(s: unknown): string {
+        return String(s)
+            .replaceAll("&", "&amp;")
+            .replaceAll("<", "&lt;")
+            .replaceAll(">", "&gt;");
+    }
+
+    list(items: FreezerItem[]): string {
+        return items
+            .map(
+                (r) => `
+<li id="item-${r.id}">
+  ${this.escape(r.name)} (${r.quantity ?? 1})
+</li>`,
+            )
+            .join("");
+    }
+}
+
+/** A Durable Object's behavior is defined in an exported Javascript class */
+export class CasaDurableObject extends DurableObject<Env> {
+    freezer: Freezer;
+    freezerRenderer: FreezerRenderer;
+
+    constructor(ctx: DurableObjectState, env: Env) {
+        super(ctx, env);
+
+        const sql = ctx.storage.sql;
+        initDatabaseSchema(sql);
+
+        this.freezer = new Freezer(sql);
+        this.freezerRenderer = new FreezerRenderer();
+    }
+
+    async freezer_getItems(): Promise<Response> {
+        const items = this.freezer.listItems();
+        const listHtml = this.freezerRenderer.list(items);
+
+        return new Response(listHtml, {
+            headers: { "Content-Type": "text/html" },
+        });
+    }
+
+    async freezer_addItem(name: string): Promise<Response> {
+        this.freezer.addItem(name);
+        const items = this.freezer.listItems();
+        const listHtml = this.freezerRenderer.list(items);
+
+        return new Response(listHtml + FreezerFormHtml, {
+            headers: { "Content-Type": "text/html" },
+        });
     }
 }
 
@@ -72,7 +109,7 @@ export default {
      * @param ctx - The execution context of the Worker
      * @returns The response to be sent back to the client
      */
-    async fetch(request, env, ctx): Promise<Response> {
+    async fetch(request, env, _ctx): Promise<Response> {
         const url = new URL(request.url);
 
         // Create a stub to open a communication channel with the Durable Object
@@ -83,19 +120,23 @@ export default {
         const stub = env.CASA_DURABLE_OBJECT.getByName(DURABLE_OBJECT_NAME);
 
         if (url.pathname === "/") {
-            return new Response(HomepageHtml, {
+            const html = (HomepageHtml as string).replace(
+                "{{ freezer-form }}",
+                FreezerFormHtml,
+            );
+            return new Response(html, {
                 headers: { "Content-Type": "text/html" },
             });
         }
 
-        if (url.pathname === "/items" && request.method === "GET") {
-            return stub.getItems();
+        if (url.pathname === "/freezer/items" && request.method === "GET") {
+            return stub.freezer_getItems();
         }
 
-        if (url.pathname === "/items" && request.method === "POST") {
+        if (url.pathname === "/freezer/items" && request.method === "POST") {
             const formData = await request.formData();
             const name = formData.get("name");
-            return stub.addItem(name);
+            return stub.freezer_addItem(name);
         }
 
         return new Response("Not found", { status: 404 });
